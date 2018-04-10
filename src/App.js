b6db0bae10e8e9a965c4d46d93ff86ceece74ffc
khanhld94 <leduykhanh.hedspi@gmail.com>
2018-04-10 15:15:09 +0900

    add socket

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from 'socket.io-client'
+
 
 class Matrix extends Component{
     constructor(props,context){
@@ -13,8 +15,20 @@ class Matrix extends Component{
         this.state={
             init_matrix: arr,
             turn: 1,
-            gameOver: false
+            gameOver: false,
+            endpoint: "https://mighty-taiga-45737.herokuapp.com",
         }
+        this.socket = socketIOClient(this.state.endpoint)
+        this.socket.on('sendData', (data) => {
+            if(data.length !== 0){
+            this.setState({
+                init_matrix: data.init_matrix,
+                turn: data.turn,
+                gameOver: data.gameOver,
+                endpoint: "http://localhost:4001"
+            })
+        }
+    })
         this.changeStatus = this.changeStatus.bind(this)
     }
     changeStatus(x,y){
@@ -22,12 +36,14 @@ class Matrix extends Component{
         let turn = this.state.turn
         if(tmp[y][x] === 0){
             tmp[y][x] = turn === 1 ? 1 : 2
-            turn = turn === 1 ? 2 : 1
-            this.setState({
+            turn = turn === 2 ? 1 : 2
+            let data = {
                 init_matrix: tmp,
-                turn: turn
-            })
-            this.checkGameOver(x,y)
+                turn: turn,
+                gameOver: this.checkGameOver(x,y)
+            }
+            this.socket.emit('sendData', data)
+            this.getData()
         }
     }
     newGame(){
@@ -63,52 +79,65 @@ class Matrix extends Component{
             (y >= 2 && y <= 10 && tmp[y][x] === tmp[y+1][x-1] && tmp[y][x] === tmp[y-1][x+1] && tmp[y][x] === tmp[y-2][x+2]) ||
             (y >= 3 && tmp[y][x] === tmp[y-1][x+1] && tmp[y][x] === tmp[y-2][x+2] && tmp[y][x] === tmp[y-3][x+3])
         ){
-            alert("player "+this.state.turn+" win")
-            this.setState({
-                gameOver: true
-            })
+            // alert("player "+this.state.turn+" win")
+            // this.setState({
+            //     gameOver: true
+            // })
+            return true
         }
+        else return false
+    }
+    getData(){
+        this.socket.on('sendData', (data) => {
+            this.setState({
+            init_matrix: data.init_matrix,
+            turn: data.turn,
+            gameOver: data.gameOver,
+        })
+    })
     }
     render(){
         let matrix_list = [];
         let matrix = this.state.init_matrix;
         for(let y in matrix){
-          for(let x in matrix[y]){
-            if(matrix[y][x] === 1){
-              matrix_list.push(<div key={[x,y]} className="tic-tac-toe-cell oSymbol" onClick={() => this.changeStatus(x,y)} ></div>)
-            }else if(matrix[y][x] === 2){
-              matrix_list.push(<div key={[x,y]} className="tic-tac-toe-cell xSymbol" onClick={() => this.changeStatus(x,y)}></div>)
-            }else{
-              matrix_list.push(<div key={[x,y]} className="tic-tac-toe-cell" onClick={() => this.changeStatus(x,y)}></div>)
+            for(let x in matrix[y]){
+                if(matrix[y][x] === 1){
+                    matrix_list.push(<div key={[x,y]} className="tic-tac-toe-cell oSymbol"></div>)
+                }else if(matrix[y][x] === 2){
+                    matrix_list.push(<div key={[x,y]} className="tic-tac-toe-cell xSymbol"></div>)
+                }else{
+                    matrix_list.push(<div key={[x,y]} className="tic-tac-toe-cell" onClick={() => this.changeStatus(x,y)}></div>)
+                }
             }
-          }
         }
         if (this.state.gameOver){
-            return(<h1 className="btn btn-primary" onClick={() => this.newGame()}>New</h1>)
+            return(
+                <h1 className="btn btn-primary" onClick={() => this.newGame()}>New</h1>
+        )
         }
         else
-        return(
-            matrix_list
-        )
+            return(
+                matrix_list
+            )
     }
 }
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <div className="App-intro">
-          <div className="tic-tac-toe--field">
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <div className="App-intro">
+            <div className="tic-tac-toe--field">
             <div className="tic-tac-toe--cells-matrix">
-              <Matrix/>
+            <Matrix/>
             </div>
-          </div>
-        </div>
-      </div>
+            </div>
+            </div>
+            </div>
     );
-  }
+    }
 }
 
 export default App;
